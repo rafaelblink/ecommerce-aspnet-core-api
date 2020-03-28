1857c7b0f2981005ed3f897de7d6145ac155df3a
Rafael Ferreira <rafaelferreira@Rafaels-MacBook-Air.local>
2020-03-28 18:20:59 -0300

    changing name of application on swagger

--- Startup.cs
+++ Startup.cs
@@ -78,7 +78,7 @@ namespace HPlusSport
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HPlus Sport");
             });
         }
     }
